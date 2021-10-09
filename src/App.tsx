d04ffde7d4454f0587fd90e07bd3537d32e62092
李士林 <358915905@qq.com>
2021-10-09 10:50:21 +0800

    3-14 update setState异步对象合并，使用

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,26 @@ import Robot from './components/Robot';
 
 import ShoppingCart from './components/ShoppingCart';
 
-interface Props {}
+interface Props { }
 
 interface State {
-  robotGallery:any[]
+  robotGallery: any[],
+  count: number
 }
-class App extends React.Component<Props,State> {
-  constructor(props:Props) {
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props)
     this.state = {
-      robotGallery: []
+      robotGallery: [],
+      count: 0
     };
-    
+
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response=>response.json())
-    .then(data=>this.setState({robotGallery:data}))
+      .then(response => response.json())
+      .then(data => this.setState({ robotGallery: data }))
   }
 
   render() {
@@ -36,6 +38,22 @@ class App extends React.Component<Props,State> {
           <img src={logo} className={styles.appLogo} alt="" />
           <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
         </div>
+        <button onClick={() => {
+          this.setState((preState,preProps)=>{
+            return { count: preState.count + 1 };
+          }, () => {
+            console.log('count', this.state.count)
+          });
+
+          this.setState((preState,preProps)=>{
+            return { count: preState.count + 1 };
+          }, () => {
+            console.log('count', this.state.count)
+          });
+          
+          
+
+        }}>count {this.state.count}</button>
         <ShoppingCart />
         <div className={styles.robotList}>
           {
@@ -44,7 +62,7 @@ class App extends React.Component<Props,State> {
         </div>
       </div>
     )
-}
+  }
 }
 
 export default App;
