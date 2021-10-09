baf902098a90d5b4bddc0082288fd1e23b81c06f
李士林 <358915905@qq.com>
2021-10-09 11:26:15 +0800

    4-3 setState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './assets/images/logo.svg'
 
 import styles from './App.module.css'
@@ -9,29 +9,11 @@ import Robot from './components/Robot';
 
 import ShoppingCart from './components/ShoppingCart';
 
-interface Props { }
 
-interface State {
-  robotGallery: any[],
-  count: number
-}
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      robotGallery: [],
-      count: 0
-    };
+const App : React.FC = (props)=>{
 
-  }
+  const [count , setCount]=useState<number>(0)
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => this.setState({ robotGallery: data }))
-  }
-
-  render() {
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
@@ -39,30 +21,17 @@ class App extends React.Component<Props, State> {
           <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
         </div>
         <button onClick={() => {
-          this.setState((preState,preProps)=>{
-            return { count: preState.count + 1 };
-          }, () => {
-            console.log('count', this.state.count)
-          });
-
-          this.setState((preState,preProps)=>{
-            return { count: preState.count + 1 };
-          }, () => {
-            console.log('count', this.state.count)
-          });
-          
-          
-
-        }}>count {this.state.count}</button>
+          setCount(count+1)
+        }}>count {count}</button>
         <ShoppingCart />
-        <div className={styles.robotList}>
+        {/* <div className={styles.robotList}>
           {
             this.state.robotGallery.map(r => <Robot id={r.id} name={r.name} email={r.email} />)
           }
-        </div>
+        </div> */}
       </div>
     )
-  }
+  
 }
 
 export default App;
